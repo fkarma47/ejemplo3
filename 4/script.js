1d950300de44fccc08d6f50386f4ad6585fde8da
Fabian_HR <fabianhurtado.13cancer@gmail.com>
2024-10-24 16:08:14 -0500

    primer commit2

---
+++ 4/script.js
@@ -0,0 +1,21 @@
+// Initialize Swiper instances
+const swiper1 = new Swiper('#swiper1', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  const swiper2 = new Swiper('#swiper2', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  const swiper3 = new Swiper('#swiper3', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
